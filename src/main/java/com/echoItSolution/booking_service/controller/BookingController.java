ae6e719f0188a40e7331bd40d4582fbbd8661b0c
ariftxt <arif.ali@xyloop.com>
2025-09-10 17:24:45 +0530

    added gateway-service

--- src/main/java/com/echoItSolution/booking_service/controller/BookingController.java
+++ src/main/java/com/echoItSolution/booking_service/controller/BookingController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("booking")
+@RequestMapping("/booking")
 public class BookingController {
     @Autowired
     private BookingService bookingService;
